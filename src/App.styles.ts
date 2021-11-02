555a750c20afb35da6133a99cb652f549028528d
Amanda Rarymi <amanda.rarymi@gmail.com>
2021-11-02 17:21:48 -0300

    🎉 NewProject

---
+++ src/App.styles.ts
@@ -0,0 +1,26 @@
+import styled from 'styled-components';
+
+export const Container = styled.div``;
+export const DividerContainer = styled.div`
+  position: relative;
+`;
+export const DividerText = styled.h1`
+  margin: 0;
+  padding: 0;
+  color: black;
+  transform: translateX(50%);
+  margin: 0;
+  padding: 0;
+  color: black;
+  position: absolute;
+  right: 50%;
+  top: 91px;
+  font-size: 50px;
+`;
+export const Body = styled.div`
+  margin: auto;
+  max-width: 980px;
+  position: absolute;
+  margin-left: 600px;
+  margin-top: -210px;
+`;
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
+import * as C from './App.styles';
+import { Item } from './types/Item';
+import { Category } from './types/Category';
+import { categories } from './data/categories';
+import { items } from './data/items';
+import { getCurrentMonth, filterListByMonth } from './helpers/dateFilter';
+import { TableArea } from './components/TableArea';
+import { InfoArea } from './components/InfoArea';
+import { InputArea } from './components/InputArea';
 
-function App() {
+const App = () => {
+  const [list, setList] = useState(items);
+  const [filteredList, setFilteredList] = useState<Item[]>([]);
+  const [currentMonth, setCurrentMonth] = useState(getCurrentMonth());
+  const [income, setIncome] = useState(0);
+  const [expense, setExpense] = useState(0);
+
+  useEffect(() => {
+    setFilteredList(filterListByMonth(list, currentMonth));
+  }, [list, currentMonth]);
+
+  useEffect(() => {
+    let incomeCount = 0;
+    let expenseCount = 0;
+
+    for (let i in filteredList) {
+      if (categories[filteredList[i].category].expense) {
+        expenseCount += filteredList[i].value;
+      } else {
+        incomeCount += filteredList[i].value;
+      }
+    }
+
+    setIncome(incomeCount);
+    setExpense(expenseCount);
+  }, [filteredList]);
+
+  const handleMonthChange = (newMonth: string) => {
+    setCurrentMonth(newMonth);
+  };
+
+  const handleAddItem = (item: Item) => {
+    let newList = [...list];
+    newList.push(item);
+    setList(newList);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <C.Container>
+      <C.DividerContainer>
+        <C.DividerText>Sistema Financeiro</C.DividerText>
+        <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 1440 320'>
+          <path
+            fill='#00bedf'
+            fill-opacity='1'
+            d='M0,288L48,245.3C96,203,192,117,288,112C384,107,480,181,576,213.3C672,245,768,235,864,192C960,149,1056,75,1152,85.3C1248,96,1344,192,1392,240L1440,288L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z'
+          ></path>
+        </svg>
+        <C.Body>
+          <InfoArea currentMonth={currentMonth} onMonthChange={handleMonthChange} income={income} expense={expense} />
+
+          <InputArea onAdd={handleAddItem} />
+
+          <TableArea list={filteredList} />
+        </C.Body>
+      </C.DividerContainer>
+    </C.Container>
   );
-}
+};
 
 export default App;
